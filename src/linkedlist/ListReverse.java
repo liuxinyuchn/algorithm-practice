f65833ae7eb97de9d3073dcdcdf58473f9d71815
liuxinyuchn <liuxinyuchn@hotmail.com>
2024-05-14 20:14:49 +0800

    feat: resolve lc 206

---
+++ src/linkedlist/ListReverse.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+// lc 206. 反转链表
+public class ListReverse {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
