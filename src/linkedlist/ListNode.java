d7392cc62e385f26cf892228db0377f4f4ecbb06
liuxinyuchn <liuxinyuchn@hotmail.com>
2024-05-14 20:13:56 +0800

    feat: add ListNode

---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+// 单链表
+public class ListNode {
+
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
